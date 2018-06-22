6f4f4e52c305220120880c58a8f61bcdc6fb3222
Michael Nisbet <nisbet.mr@gmail.com>
2018-06-22 21:12:49 +0100

    [WIP] Add constants class

---
+++ TMC/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinishMaker.TMC
+{
+    public enum Constants : int
+    {
+        MapHeaderBase = 0x11D95C
+    }
+
+    enum USConstants : int
+    {
+        MapHeaderBase = 0x11E214
+    }
+}
