b19fdbbfe972461ef5c37eee960dc0bf25f501a8
Michael Nisbet <nisbet.mr@gmail.com>
2018-08-17 00:16:42 +0100

    Reconfigure UI

--- UI/MainWindow.Designer.cs
+++ UI/MainWindow.Designer.cs
@@ -32,13 +32,17 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bottomStatusStrip = new System.Windows.Forms.StatusStrip();
             this.statusText = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.roomPanel = new System.Windows.Forms.Panel();
+            this.tilePanel = new System.Windows.Forms.Panel();
+            this.mapPanel = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.menuStrip1.SuspendLayout();
             this.bottomStatusStrip.SuspendLayout();
+            this.roomPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -55,7 +59,6 @@
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
-            this.saveToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
@@ -65,22 +68,15 @@
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenButtonClick);
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
-            this.saveToolStripMenuItem.Text = "Save";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitButtonClick);
             // 
@@ -109,11 +105,47 @@
             this.toolStrip.TabIndex = 1;
             this.toolStrip.Text = "toolStrip";
             // 
+            // roomPanel
+            // 
+            this.roomPanel.Controls.Add(this.treeView1);
+            this.roomPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.roomPanel.Location = new System.Drawing.Point(0, 49);
+            this.roomPanel.Name = "roomPanel";
+            this.roomPanel.Size = new System.Drawing.Size(200, 379);
+            this.roomPanel.TabIndex = 8;
+            // 
+            // tilePanel
+            // 
+            this.tilePanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tilePanel.Location = new System.Drawing.Point(600, 49);
+            this.tilePanel.Name = "tilePanel";
+            this.tilePanel.Size = new System.Drawing.Size(200, 379);
+            this.tilePanel.TabIndex = 9;
+            // 
+            // mapPanel
+            // 
+            this.mapPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapPanel.Location = new System.Drawing.Point(200, 49);
+            this.mapPanel.Name = "mapPanel";
+            this.mapPanel.Size = new System.Drawing.Size(400, 379);
+            this.mapPanel.TabIndex = 10;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(200, 379);
+            this.treeView1.TabIndex = 0;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mapPanel);
+            this.Controls.Add(this.tilePanel);
+            this.Controls.Add(this.roomPanel);
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.bottomStatusStrip);
             this.Controls.Add(this.menuStrip1);
@@ -125,6 +157,7 @@
             this.menuStrip1.PerformLayout();
             this.bottomStatusStrip.ResumeLayout(false);
             this.bottomStatusStrip.PerformLayout();
+            this.roomPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,11 +168,14 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.StatusStrip bottomStatusStrip;
         private System.Windows.Forms.ToolStripStatusLabel statusText;
         private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.Panel roomPanel;
+        private System.Windows.Forms.Panel tilePanel;
+        private System.Windows.Forms.Panel mapPanel;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
