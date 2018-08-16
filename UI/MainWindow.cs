daadf9c9ea8bb6640eb2159aa2d43e8784e0d202
Michael Nisbet <nisbet.mr@gmail.com>
2018-08-17 00:17:04 +0100

    Load ROM and check version

--- UI/MainWindow.cs
+++ UI/MainWindow.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using GBHL;
+using MinishMaker.TMC;
 
 namespace MinishMaker
 {
     public partial class MainWindow : Form
     {
+        private GBFile gba_;
+
+        private string fileName_ = "";
+        private RegionVersion version = RegionVersion.EU;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,18 +22,61 @@ namespace MinishMaker
 
         private void OpenButtonClick(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "GBA ROM|*.gba|All Files|*.*";
-            ofd.Title = "Select TMC EU ROM";
-            if (ofd.ShowDialog() == DialogResult.OK)
+            OpenFileDialog ofd = new OpenFileDialog
             {
-                statusText.Text = "ROM loaded: " + ofd.FileName;
+                Filter = "GBA ROMs|*.gba|All Files|*.*",
+                Title = "Select TMC EU ROM"
+            };
+
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
             }
+            LoadRom(ofd.FileName);
         }
 
         private void ExitButtonClick(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private bool IsValidRom()
+        {
+            // Determine Game region and if valid ROM
+            byte[] regionBytes = gba_.ReadBytes(0xAC, 4);
+            string region = System.Text.Encoding.UTF8.GetString(regionBytes);
+
+            if (region == "BZMP")
+            {
+                version = RegionVersion.EU;
+                return true;
+            }
+
+            if (region == "BZMJ")
+            {
+                version = RegionVersion.JP;
+                return true;
+            }
+
+            if (region == "BZME")
+            {
+                version = RegionVersion.US;
+                return true;
+            }
+            return false;
+        }
+
+        private void LoadRom(string fileName)
+        {
+            BinaryReader br = new BinaryReader(File.OpenRead(fileName));
+            gba_ = new GBFile(br.ReadBytes((int)br.BaseStream.Length));
+
+            if (!IsValidRom())
+            {
+                MessageBox.Show("Invalid TMC ROM. Please Open a valid ROM.", "Incorrect ROM",MessageBoxButtons.OK);
+            }
+            fileName_ = fileName;
+            
         }
     }
 }
