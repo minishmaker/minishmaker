96d3ef06366445a8269296e5f14ca8e3f2152af9
Mike Nisbet <nisbet.mr@gmail.com>
2019-05-30 19:36:12 +0100

    Grid box UI component
    Based from ZOLE

---
+++ UI/GridBox.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace MinishMaker.UI
+{
+    partial class GridBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridBox
+            // 
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GridBox_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GridBox_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.GridBox_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GridBox_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.GridBox_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ UI/GridBox.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Adapted from ZOLE's Grid Box by Lin22
+namespace MinishMaker.UI
+{
+    public partial class GridBox : PictureBox
+    {
+        private Color hoverColor = Color.White;
+        private Color selectedColor = Color.Red;
+        private Size selectionSize = new Size(16, 16);
+        private int selectedIndex = 0;
+        public bool canSelect = true;
+        public bool canHover = true;
+        private bool allowMultiSelection = false;
+        private Rectangle selectionRectangle = new Rectangle(0, 0, 1, 1);
+        private int hoverIndex = -1;
+        private int lastHoverIndex = -1;
+        private int startSelection = -1;
+        private Size canvas = new Size(128, 128);
+        private Pen selectionPen = new Pen(Color.Red, 4);
+        private InterpolationMode mode = InterpolationMode.NearestNeighbor;
+
+
+        public GridBox()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+        
+        [Description("The window's update mode."), Browsable(true)]
+        public InterpolationMode InterpolationMode
+        {
+            get { return mode; }
+            set { mode = value; Invalidate(); }
+        }
+
+        [Description("The hover color."), Browsable(true)]
+        public Color HoverColor
+        {
+            get { return hoverColor; }
+            set { hoverColor = value; this.Invalidate(); }
+        }
+
+        [Description("The selection color."), Browsable(true)]
+        public Color SelectionColor
+        {
+            get { return selectedColor; }
+            set { selectedColor = value; selectionPen = new Pen(selectedColor, 2); this.Invalidate(); }
+        }
+
+        [Description("The box size."), Browsable(true)]
+        public Size BoxSize
+        {
+            get { return selectionSize; }
+            set { selectionSize = value; this.Invalidate(); }
+        }
+
+        [Description("The selected index."), Browsable(true)]
+        public int SelectedIndex
+        {
+            get { return selectedIndex; }
+            set { selectedIndex = value; selectionRectangle = new Rectangle((value % (canvas.Width / selectionSize.Width)), (value / (canvas.Height / selectionSize.Height)), 1, 1); this.Invalidate(); }
+        }
+
+        [Description("Determines whether or not items can be selected."), Browsable(true)]
+        public bool Selectable
+        {
+            get { return canSelect; }
+            set { canSelect = value; this.Invalidate(); }
+        }
+
+        [Description("Determines whether or not the hoverbox will be shown."), Browsable(true)]
+        public bool HoverBox
+        {
+            get { return canHover; }
+            set { canHover = value; this.Invalidate(); }
+        }
+
+        [Description("The canvas size."), Browsable(true)]
+        public Size CanvasSize
+        {
+            get { return canvas; }
+            set { canvas = value; }
+        }
+
+        [Browsable(false)]
+        public int HoverIndex
+        {
+            get { return hoverIndex; }
+        }
+
+        [Browsable(false)]
+        public Rectangle SelectionRectangle
+        {
+            get { return selectionRectangle; }
+            set { selectionRectangle = value; this.Invalidate(); }
+        }
+
+        [Description("Determines whether or not more than one items can be selected."), Browsable(true)]
+        public bool AllowMultiSelection
+        {
+            get { return allowMultiSelection; }
+            set { allowMultiSelection = value; }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.InterpolationMode = mode;
+            try
+            {
+                base.OnPaint(e);
+            }
+            catch (Exception) { }
+        }
+
+        private void GridBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (canSelect)
+            {
+                if (!allowMultiSelection)
+                {
+                    if (selectedIndex != -1)
+                    {
+                        Point p = getIndexPoint(selectedIndex);
+                        e.Graphics.DrawRectangle(selectionPen, p.X, p.Y, selectionSize.Width, selectionSize.Height);
+                    }
+                    else
+                    {
+                        Point p = getIndexPoint(selectedIndex);
+                        e.Graphics.DrawRectangle(selectionPen, p.X, p.Y, selectionSize.Width * selectionRectangle.Width, selectionSize.Height * selectionRectangle.Height);
+                    }
+                }
+
+                if (canHover)
+                {
+                    if (hoverIndex != -1)
+                    {
+                        Point p = getIndexPoint(hoverIndex);
+                        e.Graphics.DrawRectangle(new Pen(hoverColor), p.X, p.Y, selectionSize.Width - 1, selectionSize.Height - 1);
+                    }
+                }
+            }
+        }
+
+        private Point getIndexPoint(int i)
+        {
+            int width = (canvas.Width / selectionSize.Width);
+            int height = (canvas.Height / selectionSize.Height);
+            int x = i % width;
+            int y = i / width;
+            return new Point(x * selectionSize.Width, y * selectionSize.Height);
+        }
+
+        private void GridBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x;
+            int y;
+            int width;
+            int height;
+            if (allowMultiSelection && startSelection != -1)
+            {
+                x = e.X / selectionSize.Width;
+                y = e.Y / selectionSize.Height;
+                width = x - selectionRectangle.X + 1;
+                height = y - selectionRectangle.Y + 1;
+                if (width > 0)
+                    selectionRectangle.Width = width;
+                else
+                    selectionRectangle.Width = 1;
+                if (height > 0)
+                    selectionRectangle.Height = height;
+                else
+                    selectionRectangle.Height = 1;
+                if (selectionRectangle.X + selectionRectangle.Width > canvas.Width / selectionSize.Width)
+                    selectionRectangle.Width = (canvas.Width / selectionSize.Width) - selectionRectangle.X;
+                if (selectionRectangle.Y + selectionRectangle.Height > canvas.Height / selectionSize.Height)
+                    selectionRectangle.Height = (canvas.Height / selectionSize.Height) - selectionRectangle.Y;
+                this.Invalidate();
+                return;
+            }
+
+            if (e.X < 0 || e.Y < 0 || e.X >= canvas.Width || e.Y >= canvas.Height)
+            {
+                if (hoverIndex != -1)
+                {
+                    hoverIndex = -1;
+                    lastHoverIndex = -1;
+                    this.Invalidate();
+                }
+                return;
+            }
+
+            width = (canvas.Width / selectionSize.Width);
+            height = (canvas.Height / selectionSize.Height);
+            x = e.X / selectionSize.Width;
+            y = e.Y / selectionSize.Height;
+            hoverIndex = x + y * width;
+
+            if (canHover)
+            {
+                if (lastHoverIndex != hoverIndex)
+                {
+                    lastHoverIndex = hoverIndex;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void GridBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (canHover)
+            {
+                hoverIndex = -1;
+                lastHoverIndex = -1;
+                this.Invalidate();
+            }
+        }
+
+        private void GridBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (canSelect && hoverIndex != -1)
+                {
+                    startSelection = hoverIndex;
+                    selectionRectangle = new Rectangle((e.X / selectionSize.Width), (e.Y / selectionSize.Height), 1, 1);
+                    selectedIndex = hoverIndex;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void GridBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            startSelection = -1;
+        }
+    }
+}
